60a92d02513a6fe30b48047bd227580c9865076a
michelnassarleb <michelnassar371@gmail.com>
2023-01-22 20:26:22 +0200

    Fixed returned undefined at the start of each word

--- script.js
+++ script.js
@@ -124,6 +124,10 @@ const reverseWordsInPlace = (phrase) => {
   const splittedPhrase = phrase.split(' ')
   let k = 0;
   let reversedPhraseInPlace = [];
+
+  for (let i = 0; i <= splittedPhrase.length - 1; i++) {
+    reversedPhraseInPlace[i] = ''
+  }
   for (let i = 0; i <= splittedPhrase.length - 1; i++) {
     for (let j = splittedPhrase[i].length - 1; j >= 0; j--) {
       reversedPhraseInPlace[i] += splittedPhrase?.[i]?.split('')?.[j]
