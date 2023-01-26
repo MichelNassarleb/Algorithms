77a4bd0c38256c157035253a9b81deb36fd24f7d
michelnassarleb <michelnassar371@gmail.com>
2023-01-26 21:09:58 +0200

    Added another way to sort an array of numbers

--- Q&A/Algorithm17/algorithm.js
+++ Q&A/Algorithm17/algorithm.js
@@ -1,3 +1,17 @@
-function sortArrayOfNumbers(arr) {
+const sortArrayOfNumbers = (arr) => {
     return arr.sort((a, b) => a - b)
+}
+
+const sortArrayOfNumbers1 = (arr) => {
+    let num;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] > arr[j + 1]) {
+                num = arr[j];
+                arr[j] = arr[j + 1]
+                arr[j + 1] = num
+            }
+        }
+    }
+    return arr;
 }
