c4252408308ce58e80839f230085ce5afe3880f7
michelnassarleb <michelnassar371@gmail.com>
2023-01-27 22:00:45 +0200

    Added is valid bracket algo

--- script.js
+++ script.js
@@ -236,6 +236,43 @@ const sortArrayOfNumbers1 = (arr) => {
   }
   return arr;
 }
+const isValidBracketSequence = (str) => {
+  let length = str.length
+  if (str.length == 0) return false
+  if (length % 2 != 0) return false
+  const arr = []
+  for (let i = 0; i < length; i++) {
+    if (str[i] === '(' || str[i] === ')' || str[i] === '{' || str[i] === '}' || str[i] === '[' || str[i] === ']') {
+
+      if (str[i] === '(') {
+        if (str[i + 1] === ')') {
+          arr.push('match')
+        } else {
+          arr.push('no match')
+        }
+      }
+      if (str[i] === '{') {
+        if (str[i + 1] === '}') {
+          arr.push('match')
+        } else {
+          arr.push('no match')
+        }
+      }
+      if (str[i] === '[') {
+        if (str[i + 1] === ']') {
+          arr.push('match')
+        } else {
+          arr.push('no match')
+        }
+      }
+    } else return false
+  }
+  for (let z = 0; z < arr.length; z++) {
+    if (arr[z] === 'no match') return false
+  }
+  return true
+}
+console.log(isValidBracketSequence('[(){}'))//true
 console.log(sortArrayOfNumbers1([4, 1, 2, 5]))//[1,2,4,5]
 console.log(sortArrayOfNumbers([4, 1, 2, 5]))//[1,2,4,5]
 console.log(numberOfZeros(100))//11
