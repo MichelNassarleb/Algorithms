ce62a77f3d341335d06ae778d2765b6045daf9f9
michelnassarleb <michelnassar371@gmail.com>
2023-01-29 21:32:14 +0200

    Added validate email algorithm to the script.js file

--- script.js
+++ script.js
@@ -302,6 +302,36 @@ const countAndSortArray = (arr) => {
   return `count array: [${countArray}]\nsorted Array:[${sortedArray}]`
 }
 
+const validateEmail = (str) => {
+  let foundChars = { 'contains@': 0, 'countainsDot': 0, 'count@': 0, 'DotBefore@': 0 }
+  const length = str.length;
+  if (str.length == 0) return false
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == '@') {
+      if (i != 0 && i != length - 1) {
+        foundChars['contains@'] = i;
+        foundChars['count@']++
+      } else {
+        foundChars['count@']++
+      }
+
+    }
+    if (str[i] == '.' && (i != foundChars['contains@'] + 1 && i != foundChars['contains@'] - 1) && length - 1 - i > 1) {
+      if (i > foundChars['contains@']) {
+        foundChars['countainsDot'] = i
+      } else {
+        foundChars['DotBefore@']++
+      }
+    }
+  }
+  if (str.length < 257 && foundChars['count@'] == 1 && !!foundChars['contains@'] && !!foundChars['countainsDot'] && !foundChars['DotBefore@']) {
+    return true
+  }
+  return false
+}
+
+
+console.log('is a valid email:', validateEmail('johndoe@gmail.com')) //true
 console.log(countAndSortArray([0, 0, 0, 3, 3, 3, 4, 4, 5, 5, 1, 1]))
 console.log('is valid bracket sequence', isValidBracketSequence('[(){}'))//false
 console.log('sort array method number 2', sortArrayOfNumbers1([4, 1, 2, 5]))//[1,2,4,5]
