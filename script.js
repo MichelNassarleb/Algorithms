4dec148469f157f90c94769baaffd41956966f72
michelnassarleb <michelnassar371@gmail.com>
2023-03-05 21:55:10 +0200

    Added the sum of two nums algo to the script file

--- script.js
+++ script.js
@@ -481,7 +481,36 @@ function longestUniqueSubsttr(str)
     }
     return res;
 }
+var twoSum = function(nums, target) {
+  let numsLength = nums.length
+  let arrOfValues = [0]
+  //Constraints
+  if(numsLength < 2 || numsLength >Math.pow(10,4)){
+      return [0]
+  }
+  if(target < - Math.pow(10,9) || target > Math.pow(10,9)){
+              return [0]
+          }
+  //We loop over the nums array
+  for (let i = 0;i<numsLength;i++){
+              //Constraints
+      if(nums[i] < - Math.pow(10,9) || nums[i] > Math.pow(10,9)){
+              return [0]
+          }
+      //We loop over the nums array since it's second item
+      for(let j = i+1; j < numsLength;j++){
 
+          //If the sum of the two items is equal to target, return the value immediately
+          if((nums[i] + nums[j]) == target){
+              arrOfValues.pop()
+              arrOfValues.push(i,j)
+              return arrOfValues
+          }
+      }
+  }
+};
+
+console.log('the index of the sum of two nums equal to 9 is',twoSum([0,2,7,5],9)) // [1,2]
 console.log('longest str', longestUniqueSubsttr('abcabcaa')) // 3
 
 console.log('Reversed Number of -312 is',reverse(-312)) // -213
