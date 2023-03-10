4db475f9a2a92b9ea9dad98e8df437dd44cbc607
JoeFarhat1 <michelnassar26@gmail.com>
2023-03-10 20:20:26 +0200

    Added max area algo

---
+++ Q&A/Container with most water/maxArea.js
@@ -0,0 +1,13 @@
+const maxArea = (height) =>{
+    let i = 0
+    let j = height.length - 1
+    let max = 0
+    let area
+ 
+    while(i < j){
+        area = (j-i) * Math.min(height[i],height[j])
+        max = Math.max(area,max)
+        height[i] < height[j] ? i++ : j--
+    }
+    return max
+     }
